41eba2657dd6989f96eafee89dd9e817c4a424ed
Ayokunnumi1 <omololuayk@gmail.com>
2024-04-03 19:40:57 +0100

    Initialize buttons

--- index.js
+++ index.js
@@ -77,3 +77,7 @@ const locations = [
     text: "You enter the cave. You see some monsters."
   }
 ];
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
